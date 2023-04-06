d3491db394528da5337fac10f879fe9b3e52ee3b
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-04-06 15:43:21 -0300

    add: core project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import {Poppins} from 'next/font/google'
+import {Inter} from 'next/font/google'
+
+const poppins = Poppins({
+  subsets:['latin'],
+  weight:['400', '500'],
+  variable: '--font-poppins'
+})
+
+const inter = Inter({
+  subsets:['latin'],
+  weight:['500','600'],
+  variable: '--font-inter'
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${poppins.variable} ${inter.variable} font-poppins`}>
+       <Component {...pageProps} />
+    </main>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'exclusive-background':'#FAFAFA',
+
+        'exclusive-text-1': '#FAFAFA',
+        'exclusive-text-2': '#000000',
+
+        'exclusive-button-1':'#00FF66',
+
+        'exclusive-button-2':'#DB4444',
+        'exclusive-button-2-hover':'#E07575',
+      },
+
+      fontFamily: {
+        poppins:['var(--font-poppins)', ...fontFamily.sans],
+        inter:['var(--font-inter)', ...fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
+
